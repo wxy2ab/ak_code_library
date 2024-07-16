492739ce9064d0abcf0ea16e842e783ac45b52d6
wxy2ab <wxy2ab@gmail.com>
2024-07-16 20:20:36 +0800

    update buil markdown

--- core/build_markdown.py
+++ core/build_markdown.py
@@ -4,7 +4,8 @@ import json
 def build_markdown():
     agenda_file = './json/agenda.json'
     readme_file = './README.md'
-    table_header = "# Table of Contents\n\n| 索引 | 名称 | 步骤 |\n|------|------|------|\n"
+    
+    table_header = "# Table of Contents\n\n| 索引 | 名称 | 步骤 | 参数 |\n|------|------|------|------|\n"
     table_rows = []
 
     # 读取 agenda.json
@@ -16,7 +17,8 @@ def build_markdown():
         index = item.get("index", "")
         key = item.get("key", "")
         number_of_steps = item.get("number_of_steps", "")
-        table_rows.append(f"| {index} | {key} | {number_of_steps} |")
+        params = item.get("params", "")  # 获取params字段
+        table_rows.append(f"| {index} | {key} | {number_of_steps} | {params} |")
 
     table_content = table_header + "\n".join(table_rows) + "\n"
 
@@ -27,6 +29,8 @@ def build_markdown():
     # 替换 # Table of Contents 之后的内容
     if "# Table of Contents" in readme_content:
         readme_content = readme_content.split("# Table of Contents")[0] + table_content
+    else:
+        readme_content += "\n" + table_content
 
     # 写回 README.md
     with open(readme_file, 'w', encoding='utf-8') as file:
