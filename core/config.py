20eb54912701e11fdfaaaeb5598e3a8d00570586
wxy2ab <wxy2ab@sina.com>
2024-08-07 17:53:03 +0800

    update config file

--- core/config.py
+++ core/config.py
@@ -6,6 +6,6 @@ def get_key(name:str, section:str="Default", default:str=None):
     config = configparser.ConfigParser()
     try:
         config.read('setting.ini')
-        return config.get('GitHub', 'token', fallback=default)
+        return config.get(section, name, fallback=default)
     except:
         return default
